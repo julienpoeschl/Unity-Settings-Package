27893fe58073f68c56e588ac1123f60c40bce10f
julienpoeschl <julienpoeschl@outlook.de>
2026-02-07 23:33:30 +0100

    Added settings type for strings
    - Limit is here character limit and only allows strings of certain length

---
+++ com.julienpoeschl.settings/Runtime/StringSetting.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Settings
+{
+    /// <summary>
+    /// Scriptable object setting for a string.
+    /// 
+    /// Has new property `characterLimit` which limits the amount of characters allowed for the value of the setting.
+    /// 
+    /// The `SetValue` method is overwritten to check if the new value satisfies the character limit.
+    /// </summary>
+    [CreateAssetMenu(fileName = "StringSetting", menuName = "Settings/String")]
+    public class StringSetting : GenericSetting<string>
+    {
+        [SerializeField] private Range<uint> characterLimit;
+        public Range<uint> CharacterLimit => characterLimit;
+
+        /// <summary>
+        /// Setter that only accepts values satisfying the character limit of this setting.
+        /// </summary>
+        /// <param name="newValue"></param>
+        public override void SetValue(string newValue)
+        {
+            if (characterLimit.Min > newValue.Length || characterLimit.Max < newValue.Length)
+            {
+                Debug.LogError("The new value: " + newValue + " of setting: " + name + " was outside of the limit and therefore discarded.");
+                return;
+            }
+
+            base.SetValue(newValue);
+        }
+    }
+}
